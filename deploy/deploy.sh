3c64ae5923c6b908a9eb5923048f992b142914ae
Marvin M. Agüero T <maguero@correo.ugr.es>
2017-06-22 17:02:23 +0200

    app/db ok. Ref. #1

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,5 +1,5 @@
 vagrant box add https://github.com/jose-lpa/packer-ubuntu_lts/releases/download/v3.1/ubuntu-16.04.box --name ubuntu16.04_docker
 vagrant up
 vagrant provision
-vagrant ssh -- -t pwd
+#vagrant ssh -- -t pwd
 
