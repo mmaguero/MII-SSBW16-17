bd53e17b8313afebeba68c27fe280c13329bb680
Marvin M. Agüero T <maguero@correo.ugr.es>
2017-06-24 21:14:59 +0200

    deploy casi listo

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,5 +1,13 @@
+# Up VM containers
 vagrant box add https://github.com/jose-lpa/packer-ubuntu_lts/releases/download/v3.1/ubuntu-16.04.box --name ubuntu16.04_docker
 vagrant up
+# Si da problemas al provisionar
 vagrant provision
-#vagrant ssh -- -t pwd
-
+# Borrar tmp
+vagrant ssh
+docker rm $(sudo docker ps -a | grep Exit | cut -d ' ' -f 1)
+docker rmi $(docker images | tail -n +2 | awk '$1 == "<none>" {print $'3'}')
+docker volume rm $(docker volume ls -qf dangling=true)
+exit
+vagrant ssh -- -t docker images 
+vagrant ssh -- -t docker ps -a 
