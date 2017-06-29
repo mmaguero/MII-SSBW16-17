45879a707a86d17cf799be48a14f5f3e747156cb
Marvin M. Agüero T <maguero@correo.ugr.es>
2017-06-29 17:35:37 +0200

    Allow hosts

--- venvs/sitio_web/sitio_web/settings.py
+++ venvs/sitio_web/sitio_web/settings.py
@@ -15,6 +15,7 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+os.environ['DJANGO_SETTINGS_MODULE'] = "app.settings_prod"
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
@@ -25,7 +26,7 @@ SECRET_KEY = 'k6$d%et5$+la7nvooi-e%*-q=i6f&w5eew5pxge@x^sx!@rp1r'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 CORS_ORIGIN_ALLOW_ALL = True
 
